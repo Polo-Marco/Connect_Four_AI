1fe8e1cf51f864da7cae69ad674475b5eba6e1c6
Hsing-Yuan Ma <46392073+Polo-Marco@users.noreply.github.com>
2025-06-20 13:14:09 +0800

    Align board display with column headers

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,7 @@
 #include "AI1.h"
 #include "Player.h"
 #include <cstdio>
+#include <iomanip>
 #define STUDENT 2
 using namespace std;
 
@@ -230,18 +231,37 @@ vector < vector<char> > move(vector < vector<char> > b3, int c2, char d2) {
 }
 
 void output(vector < vector<char> > b4) {
-        cout << "        1    2    3    4    5    6    7" << endl;
-        cout << "     =================================" << endl;
-	for (int i = 0; i < 6; i++) {
-		cout << "  " << i << "  |";
-		for (int j = 0; j < 7; j++) {
+        // column headers
+        cout << "     ";
+        for (int col = 1; col <= 7; ++col) {
+                cout << setw(4) << col;
+        }
+        cout << endl;
 
-			cout << b4[i][j] << "   |";;
-		}
-		cout << endl;
+        // top border
+        cout << "    +";
+        for (int i = 0; i < 7; ++i) {
+                cout << "----";
+        }
+        cout << "+" << endl;
 
-	}
-        cout << "     =================================" << endl;
+        // board rows
+        for (int i = 0; i < 6; i++) {
+                cout << "  " << i << " |";
+                for (int j = 0; j < 7; j++) {
+                        char cell = b4[i][j] ? b4[i][j] : ' ';
+                        cout << setw(3) << cell << ' ';
+                }
+                cout << "|" << endl;
+
+        }
+
+        // bottom border
+        cout << "    +";
+        for (int i = 0; i < 7; ++i) {
+                cout << "----";
+        }
+        cout << "+" << endl;
 }
 
 void printScore() {
